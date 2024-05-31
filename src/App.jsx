dbb5689e8bb9b7abf7a22ee34d61d47789943354
David <david.sanchez@tecnologicoliceocristiano.edu.ec>
2024-05-30 22:04:42 -0500

    TAREA componentes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import compImg from './assets/components.png';
 import logo from './assets/SpaceX_Logo_Black.png';
 
 import { CORE_CONCEPTS } from './data.js';
+import { CoreConcept } from '../components/CoreConcept.jsx';
 
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
@@ -21,15 +22,15 @@ function Header() {
   );
 }
 
-function CoreConcept(props){
-  return (
-    <li>
-      <img src={props.image} alt={props.title}/>
-      <h3>{props.title}</h3>
-      <p>{props.description}</p>      
-    </li>
-  );
-}
+// function CoreConcept(props){
+//   return (
+//     <li>
+//       <img src={props.image} alt={props.title}/>
+//       <h3>{props.title}</h3>
+//       <p>{props.description}</p>      
+//     </li>
+//   );
+// }
 
 function App() {
   return (
@@ -41,19 +42,13 @@ function App() {
           <h2>Core Concepts</h2>
           <ul>
             <CoreConcept 
-            title={CORE_CONCEPTS[4].title}
-            description={CORE_CONCEPTS[4].description}
-            image={CORE_CONCEPTS[4].image}
+            {... CORE_CONCEPTS[4]}
             />
             <CoreConcept 
-            title={CORE_CONCEPTS[5].title}
-            description={CORE_CONCEPTS[5].description}
-            image={CORE_CONCEPTS[5].image}
+            {... CORE_CONCEPTS[5]}
             />
             <CoreConcept 
-            title={CORE_CONCEPTS[6].title}
-            description={CORE_CONCEPTS[6].description}
-            image={CORE_CONCEPTS[6].image}
+            {... CORE_CONCEPTS[6]}
             />
           </ul>
         </section>
